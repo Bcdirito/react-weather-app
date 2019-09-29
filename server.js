a1256d54901e0a1ed1e3cb8a2ae0bbb87035a351
Bcdirito <bdirito91@gmail.com>
2019-09-29 10:27:20 -0400

    connected client to server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const path = require("path")
 const express = require("express")
-const hbs = require("hbs")
 const geocode = require("./utils/geocode")
 const forecast = require("./utils/forecast")
 
@@ -9,13 +8,6 @@ const port = process.env.PORT || 3000
 
 // Defining paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public") 
-const viewsPath = path.join(__dirname, "../templates/views")
-const partialsPath = path.join(__dirname, "../templates/partials")
-
-// Set up handlebars engine and views location
-app.set("view engine", "hbs")
-app.set("views", viewsPath)
-hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -25,10 +17,7 @@ app.listen(port, () => {
 })
 
 app.get("", (req, res) => {
-    res.render("index", {
-        title: "Weather",
-        name: "Brian DiRito"
-    })
+    res.send("Express Server Connected")
 })
 
 
